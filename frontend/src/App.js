e9dc169eafaefc7093fd5362361b62c419bdd274
Mustu19 <codewithmustu@gmail.com>
2024-02-28 09:27:07 +0530

    HomePage Components Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Layout/Navbar';
 import Footer from './components/Layout/Footer';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className='bg-gray-50  flex flex-col'>
       <Navbar />
-
-      <div className='flex-grow min-h-screen h-screen'>
-    
-      </div>
-
+      <Home />
       <Footer />
     </div>
   );
