fcd3acd3c888b4fbe96693e9836ca2365432390d
DhruvItaliya <italiyadhruv09@gmail.com>
2024-02-28 14:29:59 +0530

    driveSchema,userSchema modified

--- backend/models/driveSchema.js
+++ backend/models/driveSchema.js
@@ -1,37 +1,41 @@
 import mongoose from 'mongoose';
 
 const driveSchema = new mongoose.Schema({
-    food_name:{
-        type:[String],
-        require:true
+    food_name: {
+        type: [String],
+        required: true
     },
-    no_of_meals:{
-        type:Number,
-        require:true
+    no_of_meals: {
+        type: Number,
+        required: true
     },
-    posted_by:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User',
-        require:true
+    posted_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
-    consent:{
-        type:Boolean,
-        require:[true,"Please check consent"],
-        enum:[true]
+    consent: {
+        type: Boolean,
+        required: [true, "Please check consent"],
+        enum: [true]
     },
-    image:{
-        type:String,
-        require:[true,"Please provide an image"]
+    image: {
+        type: String,
+        required: [true, "Please provide an image"]
     },
-    contributed_by:{
-        type:[mongoose.Schema.Types.ObjectId],
+    contributed_by: {
+        type: [mongoose.Schema.Types.ObjectId],
         ref: 'User', // Reference to the User model
-        default:[]
+        default: []
     },
-    active:{
-        type:Boolean,
-        default:true
+    review_post_by: {
+        type: [Boolean],
+        default: [],
+    },
+    active: {
+        type: Boolean,
+        default: true
     }
-},{timestamps:true}); 
+}, { timestamps: true });
 
-export const Drives = new mongoose.model('Drives',driveSchema);
+export const Drives = new mongoose.model('Drives', driveSchema);
--- backend/routes/hotelsRoutes.js
+++ backend/routes/hotelsRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import { drivePost,myDrives_active,myDrives_inactive } from '../controllers/hotelsController.js';
+import { drivePost, myDrives_active, myDrives_inactive } from '../controllers/hotelsController.js';
 import { isAuthenticated } from '../middlewares/auth.js';
 
 //Destructuring body and validationResult
@@ -10,8 +10,9 @@ import { body } from 'express-validator';
 router.post('/drive_post', [
     //Validation using express-validator
     body('food_name', "Enter valid food_name!").isLength({ min: 3, max: 30 }),
-    body('no_of_meals', "Atleast 5 meals required!").isNumeric({min:5}),
-],isAuthenticated, drivePost);
+    body('no_of_meals', "Atleast 5 meals required!").isNumeric({ min: 5 }),
+], isAuthenticated, drivePost);
+
 
 router.get('/my_drives_active', isAuthenticated, myDrives_active);
 router.get('/my_drives_inactive', isAuthenticated, myDrives_inactive);
