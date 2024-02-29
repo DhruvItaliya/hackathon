f11d01a191835675036d0d752b3feb52e2f86d85
Mustu19 <codewithmustu@gmail.com>
2024-02-29 19:50:38 +0530

    Home and About Us Page Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,18 @@ import Home from './components/Home/Home';
 import Error from './components/NotFound/error';
 import Popup from './components/Layout/popup';
 import Loading from './components/Layout/loading';
-import ContactUs from './components/contactus';
-import Login from './components/login';
-import { Routes, Route, Link} from 'react-router-dom';
-import Signup from './components/signup';
-import LeaderBoard from './components/leaderboard';
+import ContactUs from './components/Contactus';
+import AboutUs from './components/AboutUs';
+import Login from './components/Login';
+import { Routes, Route, Link } from 'react-router-dom';
+import Signup from './components/Signup';
+import LeaderBoard from './components/Leaderboard';
 import PageNotFound from './components/NotFound/NotFound';
 function App() {
 
   return (
     <div className='bg-gray-50  flex flex-col'>
-      
+
       <div>
         <Navbar />
       </div>
@@ -24,12 +25,12 @@ function App() {
         <div className='bg-yellow-200 absolute animation-delay-4000 mix-blend-multiply animate-circle h-72 w-72 rounded-full top-72 filter blur-md right-12 opacity-40 '></div>
         <Routes>
 
-        <Route path="/LeaderBoard" element={<LeaderBoard/>}></Route>
-        <Route path="/ContactUs" element={<ContactUs/>}></Route>
-        {/* <Route path="/AboutUs" element={<AboutUs/>}></Route>*/}
-        <Route path="/Home" element={<Home/>}></Route>
-        {/*<Route path="/Feed" element={<Feed/>}></Route> */}
-        <Route path="*" element={<PageNotFound/>}></Route>
+          <Route path="/LeaderBoard" element={<LeaderBoard />}></Route>
+          <Route path="/ContactUs" element={<ContactUs />}></Route>
+          <Route path="/AboutUs" element={<AboutUs />}></Route>
+          <Route path="/Home" element={<Home />}></Route>
+          {/*<Route path="/Feed" element={<Feed/>}></Route> */}
+          <Route path="*" element={<PageNotFound />}></Route>
 
         </Routes>
 
@@ -39,8 +40,8 @@ function App() {
         <Footer />
       </div>
     </div>
-    
-    
+
+
 
   );
 }
--- frontend/src/components/Home/HowCanUHelp.jsx
+++ frontend/src/components/Home/HowCanUHelp.jsx
@@ -4,24 +4,32 @@ function HowCanUHelp() {
     return (
         <>
             <section class="text-gray-600 body-font">
-                <div class="container px-5 py-24 mx-auto flex flex-wrap">
-                    <div class="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto">
-                        <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
-                            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
-                        </div>
-                        <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-purple-500 text-white relative z-10 title-font font-medium text-sm">1</div>
-                        <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
-                            <div class="flex-shrink-0 w-24 h-24 bg-purple-100 text-purple-500 rounded-full inline-flex items-center justify-center">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-12 h-12" viewBox="0 0 24 24">
-                                    <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
-                                </svg>
-                            </div>
-                            <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
-                                <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">Shooting Stars</h2>
-                                <p class="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.</p>
-                            </div>
-                        </div>
-                    </div>
+    <div class="container px-5 py-24 mx-auto flex flex-wrap">
+
+        <div class="w-full text-center mb-20">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">Our Solution</h1>
+            <div class="flex mt-6 justify-center">
+                <div class="w-16 h-1 rounded-full bg-purple-500 inline-flex"></div>
+            </div>
+        </div>
+
+        <div class="w-full flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto">
+            <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
+                <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+            </div>
+            <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-purple-500 text-white relative z-10 title-font font-medium text-sm">1</div>
+            <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+                <div class="flex-shrink-0 w-24 h-24 bg-purple-100 text-purple-500 rounded-full inline-flex items-center justify-center">
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-12 h-12" viewBox="0 0 24 24">
+                        <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+                    </svg>
+                </div>
+                <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
+                    <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">Post Availability</h2>
+                    <p class="leading-relaxed">Hotel Owner posts availability of surplus food on platform.</p>
+                </div>
+            </div>
+        </div>
                     <div class="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
                         <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
                             <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
@@ -34,8 +42,8 @@ function HowCanUHelp() {
                                 </svg>
                             </div>
                             <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
-                                <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">The Catalyzer</h2>
-                                <p class="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.</p>
+                                <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">Receive Notifications</h2>
+                                <p class="leading-relaxed">Volunteers receive city-wise notifications for available drives.</p>
                             </div>
                         </div>
                     </div>
@@ -52,8 +60,8 @@ function HowCanUHelp() {
                                 </svg>
                             </div>
                             <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
-                                <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">The 400 Blows</h2>
-                                <p class="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.</p>
+                                <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">Confirm Completion</h2>
+                                <p class="leading-relaxed">Drive completion confirmed by hotel owner upon food pickup.</p>
                             </div>
                         </div>
                     </div>
@@ -70,11 +78,69 @@ function HowCanUHelp() {
                                 </svg>
                             </div>
                             <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
-                                <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">Neptune</h2>
-                                <p class="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.</p>
+                                <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">Verify photos</h2>
+                                <p class="leading-relaxed">Volunteers post distribution photos for verification.</p>
                             </div>
                         </div>
                     </div>
+
+                    <div class="flex relative pb-10 sm:items-center md:w-2/3 mx-auto">
+                        <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
+                            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                        </div>
+                        <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-purple-500 text-white relative z-10 title-font font-medium text-sm">5</div>
+                        <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+                            <div class="flex-shrink-0 w-24 h-24 bg-purple-100 text-purple-500 rounded-full inline-flex items-center justify-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-12 h-12" viewBox="0 0 24 24">
+                                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                                    <circle cx="12" cy="7" r="4"></circle>
+                                </svg>
+                            </div>
+                            <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
+                                <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">Award Rewards</h2>
+                                <p class="leading-relaxed">Reward points and badges awarded, showcased on leaderboard.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="flex relative pb-10 sm:items-center md:w-2/3 mx-auto">
+                        <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
+                            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                        </div>
+                        <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-purple-500 text-white relative z-10 title-font font-medium text-sm">6</div>
+                        <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+                            <div class="flex-shrink-0 w-24 h-24 bg-purple-100 text-purple-500 rounded-full inline-flex items-center justify-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-12 h-12" viewBox="0 0 24 24">
+                                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                                    <circle cx="12" cy="7" r="4"></circle>
+                                </svg>
+                            </div>
+                            <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
+                                <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">Provide Reports</h2>
+                                <p class="leading-relaxed">Provide detailed drive and food distribution reports.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="flex relative pb-10 sm:items-center md:w-2/3 mx-auto">
+                        <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
+                            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                        </div>
+                        <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-purple-500 text-white relative z-10 title-font font-medium text-sm">7</div>
+                        <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+                            <div class="flex-shrink-0 w-24 h-24 bg-purple-100 text-purple-500 rounded-full inline-flex items-center justify-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-12 h-12" viewBox="0 0 24 24">
+                                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                                    <circle cx="12" cy="7" r="4"></circle>
+                                </svg>
+                            </div>
+                            <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
+                                <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">Offer Insights</h2>
+                                <p class="leading-relaxed">Offer insights for reducing food wastage to Hotel owners.</p>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </section>
         </>
